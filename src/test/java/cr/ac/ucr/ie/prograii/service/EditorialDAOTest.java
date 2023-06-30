d98746871a43de43c9db43b29590b481cbfdbe92
luisballar <luisballar123@gmail.com>
2023-06-29 19:03:17 -1100

    Editorial, terminar test, revisar constructores DAO

--- src/test/java/cr/ac/ucr/ie/prograii/service/EditorialDAOTest.java
+++ src/test/java/cr/ac/ucr/ie/prograii/service/EditorialDAOTest.java
@@ -1,10 +1,14 @@
 package cr.ac.ucr.ie.prograii.service;
 
 import cr.ac.ucr.ie.prograii.model.Editorial;
+import cr.ac.ucr.ie.prograii.model.Libro;
+import cr.ac.ucr.ie.prograii.model.Tematica;
 import org.jdom2.JDOMException;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 class EditorialDAOTest {
@@ -24,27 +28,39 @@ class EditorialDAOTest {
         }
     }
 
-        @Test
-    void guardarTematica_fuciona(){
-        Editorial editorial1 = new Editorial(01, "Monge", "Jalisco",14,);
+    @Test
+    void guardarEditorial_fuciona(){
+            List<Libro> libros = new ArrayList<Libro>();
+
+            // Primer libro
+            Libro libro1 = new Libro();
+            libro1.setIdLibro(1);
+            libro1.setTitulo("El señor de los anillos");
+            libro1.setIsbn("9788445004954");
+            libro1.setAutores(new ArrayList<>()); // Añade autores a la lista de autores
+            libro1.getItems(); // Añade items a la lista de items
+            libro1.setEditorial(new Editorial()); // Asigna una editorial
+            libro1.setTematica(new Tematica()); // Asigna una temática
+            libros.add(libro1);
+
+        Editorial editorial1 = new Editorial(01, "Monge","Jalisco",libros);
+        Editorial editorial2 = new Editorial(02, "Casa Azul","New York",libros);
 
         try {
-            TematicaDAO tematicaDAO = TematicaDAO.abrirDocumento("tematicas.xml");
-            tematicaDAO.insertarTematica(tematica1);
-            tematicaDAO.insertarTematica(tematica2);
-            tematicaDAO.insertarTematica(tematica3);
-            tematicaDAO.insertarTematica(tematica4);
+            EditorialDAO editorialDAO = EditorialDAO.abrirDocumento("editorial.xml");
+            editorialDAO.insertarEditorial(editorial1);
+            editorialDAO.insertarEditorial(editorial2);
         } catch (IOException | JDOMException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    void getTematica_funciona(){
+    void getTEditorial_funciona(){
         try {
-            TematicaDAO tematicaDAO = TematicaDAO.abrirDocumento("tematicas.xml");
-            List<Tematica> tematicas = tematicaDAO.getTematicas();
-            System.out.println(tematicas.toString());
+            EditorialDAO editorialDAO = EditorialDAO.abrirDocumento("editorial.xml");
+            List<Editorial> editorials = editorialDAO.getEditoriales();
+            System.out.println(editorials.toString());
         } catch (IOException | JDOMException e) {
             throw new RuntimeException(e);
         }
